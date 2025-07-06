29d0f286f895f1074b31d4e8f7f3abf1c2443c7e
Maria Eduarda <dud.farias@gmail.com>
2025-07-06 19:16:40 -0300

    Alterando o para o código correto

--- Calculadora.py
+++ Calculadora.py
@@ -1,9 +1,15 @@
-name = input("Hello! What is your name? ")
-print(f"Nice to meet you, {name}!")
-age_input = input("How old are you? ")
-age = int(age_input)
-bot_age = 3
-age_difference = age - bot_age
-print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
-color = input("What's your favorite color? ")
-print(f"Oh, {color} is a beautiful color!")
+print("=== Calculadora de Entregas Lorenzon ===")
+
+entrega = int(input("Quantas entregas você fez hoje? "))
+entrega4 = int(input(f"Dessas {entrega} entregas, quantas foram de menos de 3km? "))
+entrega6 = int(input(f"Dessas {entrega} entregas, quantas foram de mais de 3km? "))
+
+calc4 = entrega4 * 4
+calc6 = entrega6 * 6
+calcent = calc4 + calc6
+calctotal = calcent + 60
+
+print("Perfeito Duda. Vamos aos calculos...")
+print(f"Voce realizou {entrega4} entregas de -3km, que totalizou R${calc4}")
+print(f"Você realizou {entrega6} entregas de +3km, que totalizou R${calc6}")
+print(f"O valor total do seu dia foi de R${calctotal}")
